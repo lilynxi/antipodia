a6b8e2b4ae766246a97b34b589aad83bd70cbb17
Lisa Mueller <lisa.mueller@amazee.com>
2017-03-02 12:36:31 +0000

    make maps dumb again

--- app/screens/App/screens/AntipodePage/app.js
+++ app/screens/App/screens/AntipodePage/app.js
@@ -2,19 +2,77 @@
 
 import React, { Component } from 'react';
 import Map from './map';
+import { connect } from 'react-redux';
+import { addMarker } from '../../../../shared/state/actions/actions';
+import uuid from 'uuid';
 
 
-// const initialCenter = { lat: -25.363882, lng: 131.044922 };
+
+//
+// calculate antipode lat & lng
+//
+// const calcLatLng = (newPosition) => {
+//
+//   const antipodeLat = -newPosition.lat;
+//   let antipodeLng;
+//
+//   if(newPosition.lng<=0){
+//     antipodeLng = 180+newPosition.lng;
+//   } else {
+//     antipodeLng = -(180-newPosition.lng);
+//   }
+//   return {
+//     lat: antipodeLat,
+//     lng: antipodeLng,
+//   }
+// }
 
 
 
-const App = () => {
-  return (
-    <div>
-      <Map />
-      <Map />
-    </div>
-  )
+  // handleMapClick = (event) => {
+  //   const {dispatch} = this.props;
+  //
+  //   const newPosition = {
+  //     lat: event.latLng.lat(),
+  //     lng: event.latLng.lng(),
+  //   };
+  //
+  //   const newMarker = {
+  //     position: newPosition,
+  //     defaultAnimation: 2,
+  //     key: uuid.v4(),
+  //   }
+  //
+  //   const newAntipodePosition = calcLatLng(newPosition);
+  //
+  //   dispatch(addMarker(newMarker));
+  // }
+
+
+
+
+
+
+class App extends Component {
+
+  handleClick = () => {
+    console.log('click');
+  }
+
+  render() {
+    const {dispatch, state} = this.props;
+
+    if (!state) {
+    	return null;
+    }
+
+    return (
+      <div>
+        <Map markers={state.markers} initCenter={state.markers[0].position} handleClick={this.handleClick}/>
+        <Map markers={state.markers} initCenter={state.markers[0].position}/>
+      </div>
+    )
+  }
 }
 
 
@@ -23,4 +81,11 @@ const App = () => {
 // const getMarker = () => [lat, lon];
 // const setMarker = () => originalSetMarker(inverse(lat), inverse(lon))
 
-export default App;
+// export default App;
+
+
+const AppWithStateFromRedux = connect(state => ({
+  state
+}))(App);
+
+export default AppWithStateFromRedux;
--- app/screens/App/screens/AntipodePage/map.js
+++ app/screens/App/screens/AntipodePage/map.js
@@ -3,10 +3,42 @@
 import React, { Component } from 'react';
 import withScriptjs from "react-google-maps/lib/async/withScriptjs";
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-import { connect } from 'react-redux';
-import { addMarker } from '../../../../shared/state/actions/actions';
-import uuid from 'uuid';
 
+
+
+class Map extends Component {
+
+  handleMapLoad = (map) => {
+    this._mapComponent = map;
+    if (map) { console.log("zoom",map.getZoom()); }
+  }
+
+  onMapClick = () => {
+    //console.log("aset",this.props.handleClick);
+    // koordinaten rausholen
+    this.props.handleClick();
+  }
+
+  render() {
+    return (
+      <div style={{ height: `300px`, maxWidth:`400px` }}>
+        <GoogleMapWrapper
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp"
+          loadingElement={<div>Loading</div>}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          initialCenter={this.props.initCenter}
+          markers={this.props.markers}
+          onMapLoad={this.handleMapLoad}
+          onMapClick={this.onMapClick}
+        />
+      </div>
+    )
+  }
+}
+
+
+export default Map;
 
 
 
@@ -27,88 +59,10 @@ const GoogleMapWrapper = withScriptjs(withGoogleMap ( props =>  (
 
 
 
-//
-// calculate antipode lat & lng
-//
-const calcLatLng = (newPosition) => {
-
-  const antipodeLat = -newPosition.lat;
-  let antipodeLng;
-
-  if(newPosition.lng<=0){
-    antipodeLng = 180+newPosition.lng;
-  } else {
-    antipodeLng = -(180-newPosition.lng);
-  }
-  return {
-    lat: antipodeLat,
-    lng: antipodeLng,
-  }
-}
 
 
 
-class Map extends Component {
-
-
-  handleMapLoad = (map) => {
-    this._mapComponent = map;
-    if (map) { console.log("zoom",map.getZoom()); }
-  }
-
-
-  handleMapClick = (event) => {
-    const {dispatch} = this.props;
-
-    const newPosition = {
-      lat: event.latLng.lat(),
-      lng: event.latLng.lng(),
-    };
-
-    const newMarker = {
-      position: newPosition,
-      defaultAnimation: 2,
-      key: uuid.v4(),
-    }
-
-    const newAntipodePosition = calcLatLng(newPosition);
-
-    dispatch(addMarker(newMarker));
-  }
-
-
-
-  render() {
-    const {dispatch, state} = this.props;
-
-    if (!state) {
-			return null;
-		}
-
-    return (
-      <div style={{ height: `300px`, maxWidth:`400px` }}>
-        <GoogleMapWrapper
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp"
-          loadingElement={<div>Loading</div>}
-          containerElement={<div style={{ height: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-          initialCenter={state.markers[0].position}
-          markers={state.markers}
-          onMapLoad={this.handleMapLoad}
-          onMapClick={this.handleMapClick}
-        />
-      </div>
-    )
-  }
-}
-
-const withStateFromRedux = connect(state => ({
-  state
-}));
-
-const MapWithStateFromRedux = withStateFromRedux(Map);
-
-export default MapWithStateFromRedux;
+// onMapClick={this.handleMapClick}
 
 // export default connect(state => ({
 //   state
