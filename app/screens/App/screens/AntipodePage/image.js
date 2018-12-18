3429d51d329f33aeeceefeefc992a10e4b211592
Lisa Müller <lisa.mueller47@gmail.com>
2018-12-18 22:37:13 +0100

    add key-parameter to google streetview component

--- app/screens/App/screens/AntipodePage/image.js
+++ app/screens/App/screens/AntipodePage/image.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 const Image = ({ address }) => (
   //<div>google streetview</div>
-  <img src={`http://maps.googleapis.com/maps/api/streetview?size=640x480&location=${address}&fov=120&heading=235&pitch=10&sensor=false`} />
+  <img src={`http://maps.googleapis.com/maps/api/streetview?size=640x480&location=${address}&fov=120&heading=235&pitch=10&sensor=false&key=AIzaSyAGQXIUdvl7fkKKqt-njiiY9AygNSM9P0E`} />
 );
 
 export default Image;
